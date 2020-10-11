dfe8a18cfc4859608a0eebe1aad5f584d0541393
リンKei <72475572+ZousakeRin@users.noreply.github.com>
2020-10-11 15:42:01 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const axios = require("axios")
-const urls = ["https://foamy-wise-marscapone.glitch.me"]
+const urls = ["https://project-bot-rinkei.glitch.me"]
 setInterval(function() {
             urls.forEach(url => {
             axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
